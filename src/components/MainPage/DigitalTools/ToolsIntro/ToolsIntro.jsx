1195c3214833a71c4de3effff3e0150ffd744c9c
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-04-01 14:17:46 +0600

    added Tools Intro buttons toggle

--- src/components/MainPage/DigitalTools/ToolsIntro/ToolsIntro.jsx
+++ src/components/MainPage/DigitalTools/ToolsIntro/ToolsIntro.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import { useState } from "react";
 
 const ToolsIntro = () => {
+  const [active, setActive] = useState("products");
+
   return (
     <div className="max-w-[600px] mx-auto flex flex-col gap-4 items-center text-center">
       <h1 className="title text-5xl font-extrabold">Premium Digital Tools</h1>
@@ -9,10 +11,20 @@ const ToolsIntro = () => {
         productivity and creativity.
       </p>
       <div className="flex gap-5">
-        <button className="btn text-[.9rem] font-semibold bg-gradient-to-r from-[#4F39F6] to-[#9514FA] text-white rounded-3xl hover:from-white hover:to-white hover:text-purple-700 transition transform ease-in hover:border-purple-800 px-6">
+        <button
+          onClick={() => {
+            setActive("products");
+          }}
+          className={`btn text-[.9rem] font-semibold ${active === "products" ? `bg-gradient-to-r from-[#4F39F6] to-[#9514FA] text-white hover:from-[#9514FA] hover:to-[#4F39F6]  hover:border-purple-800` : `text-purple-600  border-purple-800`} px-6 rounded-3xl transition transform  ease-in `}
+        >
           Products
         </button>
-        <button className="btn text-purple-600 rounded-3xl px-6 hover:border-purple-800">
+        <button
+          onClick={() => {
+            setActive("cart");
+          }}
+          className={`btn text-[.9rem] font-semibold ${active === "cart" ? `bg-gradient-to-r from-[#4F39F6] to-[#9514FA] text-white hover:from-[#9514FA] hover:to-[#4F39F6]  hover:border-purple-800` : `text-purple-600  border-purple-800`} px-6 rounded-3xl transition transform  ease-in `}
+        >
           Cart (0)
         </button>
       </div>
